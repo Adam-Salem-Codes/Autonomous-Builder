17b6a691371dad63bdf77e6c483d36e73bd2dbcd
Void <84548368+ItzVoidYT@users.noreply.github.com>
2022-04-21 18:52:34 -0400

    Delete Autonomous.cpp

--- build/Autonomous.cpp
+++
@@ -1,9 +0,0 @@
-#include "vex.h"
- #include "methods.h" 
- using namespace vex; 
- int main() {
-vexcodeInit(); 
- Drivetrain.driveFor(forward,72.02881152460985, i);
-
- Drivetrain.turnFor(90.0, degrees);
-}
